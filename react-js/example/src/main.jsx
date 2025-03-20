9ea5e36238555958e196460f77004e9217757ab1
hirusha-adi <hirushaadi@gmail.com>
2025-03-20 09:35:29 +0530

    add tab spoofer

--- react-js/example/src/main.jsx
+++ react-js/example/src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// import `TabSpoofer` from the file inside the `components` folder
+import TabSpoofer from './components/TabSpoofer.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+
+    {/* setup TabSpoofer component */}
+    <TabSpoofer />
+    
   </StrictMode>,
 )
